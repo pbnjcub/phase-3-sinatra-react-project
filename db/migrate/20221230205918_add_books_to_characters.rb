7fb7f1a9cf57c48de6d91a70452ce0b6a051b0b4
Paul Jung <pbnjcub@gmail.com>
2022-12-30 16:07:14 -0500

    add book_ids column to characters

--- db/migrate/20221230205918_add_books_to_characters.rb
+++ db/migrate/20221230205918_add_books_to_characters.rb
@@ -1,4 +1,5 @@
 class AddBooksToCharacters < ActiveRecord::Migration[6.1]
   def change
+    add_column :characters, :book_ids, :integer, array:true, default: []
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_30_201719) do
+ActiveRecord::Schema.define(version: 2022_12_30_205918) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_12_30_201719) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_ids"
   end
 
 end
