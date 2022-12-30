a6b839e5576a2f5c99bc0316e4f2b33be3518d85
Paul Jung <pbnjcub@gmail.com>
2022-12-30 16:10:06 -0500

    rename book_ids to book_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_30_205918) do
+ActiveRecord::Schema.define(version: 2022_12_30_210823) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_12_30_205918) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "book_ids"
+    t.integer "book_id"
   end
 
 end
