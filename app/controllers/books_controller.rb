458f83ed7186bc5a93abfad2e7df2ec0f0fe8ee9
Paul Jung <pbnjcub@gmail.com>
2023-01-02 20:06:53 -0500

    refactor controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,37 +4,50 @@ class BooksController < ApplicationController
     end
 
     get "/books/:id" do
-        book = Book.find_by_id(params["id"])
-        book.to_json(include: [characters: {only: [:id, :name, :description]}], except: [:created_at, :updated_at])
+        find_book
+        character_to_json
     end
 
     post "/books" do
-        book = Book.new(params)
-        if book.save
+        @book = Book.new(params)
+        if @book.save
             #return object as json if saved
-            book.to_json(include: [characters: {only: [:id, :name, :description]}], except: [:created_at, :updated_at])
+            character_to_json
         else
             #return error messages if not saved
-            { errors: book.errors.full_messages }.to_json
+            character_error_messages
         end
     end
 
     patch "/books/:id" do
-        book = Book.find_by_id(params["id"])
-        if book.update(params)
-            book.to_json(include: [characters: {only: [:id, :name, :description]}], except: [:created_at, :updated_at])
+        find_book
+        if @book.update(params)
+            character_to_json
         else
-            { errors: book.errors.full_messages }.to_json
+            character_error_messages
         end
     end
 
     delete "/books/:id" do
-        book = Book.find_by_id(params["id"])
-        if book
-            book.destroy
-            book.to_json
+        find_book
+        if @book
+            @book.destroy
+            @book.to_json
         else
             { errors: ["Book Doesn't Exist"]}.to_json
         end
     end
+
+    Private
+        def find_book
+            @book = Book.find_by_id(params["id"])
+        end
+
+        def character_to_json
+            @book.to_json(include: [:characters], except: [:created_at, :updated_at])
+        end
+
+        def character_error_messages
+            { errors: @book.errors.full_messages }.to_json
+        end
 end
--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,48 +4,58 @@ class CharactersController < ApplicationController
     end
 
     get "/characters/:id" do
-        character = Character.find_by_id(params["id"])
-        character.to_json(except: [:created_at, :updated_at])
+        find_character
+        character_to_json
     end
 
-    get "/characters/:location" do
-        characters = []
-        Character.find_each do |character|
-            characters << character.where("location = ?", params[:location])
-        end
-        characters.to_json
-    end
+    # get "/characters/:location" do
+    #     # binding.pry
+    #     Character.where("location = params[:location]").to_json
+    # end
 
 
     post "/characters" do
-        character = Character.new(params)
-        if character.save
+        @character = Character.new(params)
+        if @character.save
             #return object as json if saved
-            character.to_json(include: [books: {only: [:id, :title]}], except: [:created_at, :updated_at])
+            character_to_json
         else
             #return error messages if not saved
-            { errors: character.errors.full_messages }.to_json
+            character_error_messages
         end
     end
 
     patch "/characters/:id" do
-        character = Character.find_by_id(params["id"])
-        if character.update(params)
-            character.to_json(include: [books: {only: [:id, :title]}], except: [:created_at, :updated_at])
+        find_character
+        if @character.update(params)
+            character_to_json
         else
-            { errors: character.errors.full_messages }.to_json
+            character_error_messages
         end
     end
 
     delete "/characters/:id" do
-        character = Character.find_by_id(params["id"])
-        if character
-            character.destroy
-            character.to_json
+        find_character
+        if @character
+            @character.destroy
+            @character.to_json
         else
             { errors: ["Character Doesn't Exist"]}.to_json
         end
     end
+
+    Private
+        def find_character
+            @character = Character.find_by_id(params["id"])
+        end
+
+        def character_to_json
+            @character.to_json(include: [:books], except: [:created_at, :updated_at])
+        end
+
+        def character_error_messages
+            { errors: @character.errors.full_messages }.to_json
+        end
 
 
 end
