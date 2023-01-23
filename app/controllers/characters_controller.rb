b6b0efcf3d9d020d62c2012a2fa55514379d2260
Paul Jung <pbnjcub@gmail.com>
2023-01-23 15:00:01 -0500

    add character functionality

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CharactersController < ApplicationController
     get "/characters" do
         Character.all.to_json(include: [:books], except: [:created_at, :updated_at])
@@ -13,20 +15,8 @@ class CharactersController < ApplicationController
         @book.characters.to_json
     end
 
-    post "/characters" do
-        @character = Character.new(params)
-        if @character.save
-            #return object as json if saved
-            character_to_json
-        else
-            #return error messages if not saved
-            character_error_messages
-        end
-    end
-
-    # post "/books/:book_id/characters" do
-    #     find_book
-    #     @character = @book.characters.new(params)
+    # post "/characters" do
+    #     @character = Character.new(params)
     #     if @character.save
     #         #return object as json if saved
     #         character_to_json
@@ -35,6 +25,18 @@ class CharactersController < ApplicationController
     #         character_error_messages
     #     end
     # end
+
+    post "/characters" do
+        @character = Character.new({name: params[:name], description: params[:description], location: params[:location]})
+        if @character.save
+            #return object as json if saved
+            @bookcharacter = Bookcharacter.create(character_id: @character.id, book_id: params[:book_id])
+            character_to_json
+        else
+            #return error messages if not saved
+            bookcharacter_error_messages
+        end
+    end
 
     patch "/characters/:id" do
         find_character
@@ -64,11 +66,17 @@ class CharactersController < ApplicationController
             @character.to_json(include: [:books], except: [:created_at, :updated_at])
         end
 
+        def bookcharacter_to_json
+            @bookcharacter.to_json
+        end
+
         def character_error_messages
             { errors: @character.errors.full_messages }.to_json
         end
 
-        def find_book
-            @book = Book.find_by_id(params["book_id"])
+        def bookcharacter_error_messages
+            { errors: @bookcharacter.errors.full_messages }.to_json
         end
+
+
 end
